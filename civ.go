98d7fc8dbeb9d8dbaf5866d014cc42b09d2b6762
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2019-01-30 21:27:33 +0200

    format correction

--- civ.go
+++ civ.go
@@ -5,15 +5,19 @@ import	(
     "testing"
 	"github.com/stretchr/testify/assert"
 )
+
+var welcomeMessage = "Hello worldbuilders, this is a civilizing process to find peace..."
+
 func main() {  
-	var welcomeMessage string
-	welcomeMessage = "Hello worldbuilders, this is a civilizing process to find peace."
+	
 	fmt.Println(welcomeMessage)
+
 }
 
-func TestSomething(s string) {
-
-	assert.Equals(s, "Hello worldbuilders, this is a civilizing process to find peace."
-	, "Correct message!")
+func testMessage(t *testing.T) {
+	
+	assert.Equal(t, welcomeMessage, 
+		"Hello worldbuilders, this is a civilizing process to find peace..." ,
+	 "Correct message!")
   
-  }
+}
