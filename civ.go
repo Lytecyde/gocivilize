9c1ecb8375cd2bf602cafd3b27c7e80033765f79
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2019-01-30 21:51:44 +0200

    code cleanup

--- civ.go
+++ civ.go
@@ -10,15 +10,11 @@ import (
 const welcomeMessage = "Hello worldbuilders, this is a civilizing process to find peace..."
 
 func main() {
-
 	fmt.Println(welcomeMessage)
-
 }
 
 func testMessage(t *testing.T) {
-
 	assert.Equal(t, welcomeMessage,
 		"Hello worldbuilders, this is a civilizing process to find peace...",
 		"Correct message!")
-
 }
