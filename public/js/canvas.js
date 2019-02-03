294eeea614445491090ba00893d972278590877c
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2019-02-03 13:32:35 +0200

    refactored canvas and createTabs js

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,35 +1,4 @@
 //create hex for a canvas
-function drawCanvas(){
+function drawCanvas() {
     getHexGridWH();
 }
-
-   
-function createTabs(){
-    var tabs = document.querySelectorAll('.tab-box li a');
-   
-
-    for(i = 0; i < tabs.length; i++) {
-        var tab = tabs[i];
-        setTabHandler(tab, i);
-    }
-    
-}
-
-function setTabHandler(tab, tabPos) {
-    var panels = document.querySelectorAll('article');
-
-    tab.onclick = function() {
-        for(i = 0; i < tabs.length; i++) {
-        tabs[i].className = '';
-        }
-
-        tab.className = 'active';
-
-        for(i = 0; i < panels.length; i++) {
-        panels[i].className = '';
-        }
-
-        panels[tabPos].className = 'active-panel';
-    }
-}
-    
---
+++ public/js/createTabs.js
@@ -0,0 +1,21 @@
+function createTabs() {
+    var tabs = document.querySelectorAll('.tab-box li a');
+    var tab;
+    for (i = 0; i < tabs.length; i++) {
+        tab = tabs[i];
+        setTabHandler(tab, i);
+    }
+}
+function setTabHandler(tab, tabPos) {
+    var panels = document.querySelectorAll('article');
+    tab.onclick = function () {
+        for (i = 0; i < tabs.length; i++) {
+            tabs[i].className = '';
+        }
+        tab.className = 'active';
+        for (i = 0; i < panels.length; i++) {
+            panels[i].className = '';
+        }
+        panels[tabPos].className = 'active-panel';
+    };
+}
