275dfaded1bfa5e85188bba58cff2c697ec8878b
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2019-02-06 22:38:48 +0200

    refactored,  removed duplicate functionality

--- public/js/minimap.js
+++ public/js/minimap.js
@@ -5,21 +5,6 @@ var height = parseFloat(8.660254037844388);
 
 createMinimap();
 
-function findHexWithWidthAndHeight(width, height) {
-    var y = height / 2.0;
-    //solve quadratic
-    var a = -3.0;
-    var b = (-2.0 * width);
-    var c = (Math.pow(width, 2)) + (Math.pow(height, 2));
-    var z = (-b - Math.sqrt(Math.pow(b, 2) - (4.0 * a * c))) / (2.0 * a);
-
-    var x = (width - z) / 2.0;
-
-    HT.Hexagon.Static.WIDTH = width;
-    HT.Hexagon.Static.HEIGHT = height;
-    HT.Hexagon.Static.SIDE = z;
-}
-
 function createMinimap() {
     var canvas = document.getElementById("minimap");
     var ctx = canvas.getContext('2d');
