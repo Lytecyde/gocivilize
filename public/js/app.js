2e165752aa099a3a82458b8e07078fe84ebf0b79
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2019-02-06 23:46:51 +0200

    app refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 "use strict";
 
-var grid = new HT.Grid(1900, 800);
+const MINIMAP = 0;
+const MAP = 1;
+const WORLDMAP = 2;
 
 function onload() {
     createTabs();
     createMinimap();
+    drawHexGrid("map");
 }
 
 function createTabs() {
@@ -29,12 +32,41 @@ function setTabHandler(tab, tabPos) {
     };
 }
 
-function drawHexGrid() {
-    var canvas = document.getElementById("map");
+function drawHexGrid(maptype) {
+    var canvas = document.getElementById(gridType.id[mapid]);
     var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, 1900, 800);
-    console.log(grid.Hexes.length);
-    for (var h in grid.Hexes) {
-        grid.Hexes[h].draw(ctx);
+    ctx.clearRect(0, 0, gridType.gridWidth, gridType.gridHeight);
+    console.log(gridType.mapType.length);
+    for (var h in gridType.mapType) {
+        gridType.mapType[h].draw(ctx);
+    }
+}
+
+function gridType(n) {
+    function id(n) {
+        var id = ["minimap", "map", "worldmap"];
+        return id[n];
+    }
+
+    function hexSizes(n) {
+        var size = [];
+        var gridWidth = [1900, 200];
+        var gridHeight = [800, 150];
+        size.push(gridWidth[n]);
+        size.push(gridHeight[n])
+        return size;
+    }
+
+    function hexSizes(n) {
+        var size = [];
+        var width = [parseFloat(10.0), parseFloat(100.0)];
+        var height = [parseFloat(8.660254037844388), parseFloat(86.60254037844388)];
+        size.push(width[n]);
+        size.push(height[n])
+        return size;
+    }
+
+    function mapHexes(n) {
+        return new HT.Grid(gridWidth[n], gridHeight[n]).Hexes;
     }
 }
