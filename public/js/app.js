5c28c3d936515c35c5e18e93d461a71c7714f33e
Tanel Lebedev <tanel.lebedev@gmail.com>
2019-02-23 21:13:51 +0200

    remove move.js

--- public/js/app.js
+++ public/js/app.js
@@ -166,3 +166,26 @@ function createFogMap() {
         }
     }
 }
+
+var moves = {
+    NE: 0,
+    SE: 1,
+    S: 2,
+    SW: 3,
+    NW: 4,
+    N: 5
+};
+
+function move(location, direction) {
+    removeUnit(location);
+    var newLocation = getNewLocation(location, direction);
+    drawUnit(newLocation);
+};
+
+function removeUnit(location) {
+
+};
+
+function getNewLocation(location) {
+
+};
