7573df4081f05def56b1160983cb22fe0daab45f
miku <mik.seljamaa.nagaoka@gmail.com>
2019-03-17 14:53:48 +0200

    minor lint fix

--- public/js/civilization.js
+++ public/js/civilization.js
@@ -37,5 +37,5 @@ var unit = {
 
 var civilizations = {
     names: ["Egypt", "Mesopotamia", "Greece", "Rome", "Germany", "England", "Russia", "India", "China", "Japan", "Aztec", "Maya"],
-    capitals: ["Thebes", "Uruk", "Athens", "Rome", "Berlin", "London", "Moscow", "Delhi", "Beijing", "Tokyo", "Tenochtitlan", "Tikal"],
+    capitals: ["Thebes", "Uruk", "Athens", "Rome", "Berlin", "London", "Moscow", "Delhi", "Beijing", "Tokyo", "Tenochtitlan", "Tikal"]
 };
